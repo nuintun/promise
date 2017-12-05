0639333dbc975eabd5ce33cbb63408933ecff7ab
nuintun <nuintun@gmail.com>
2017-12-05 22:23:55 +0800

    :art: Update comments

--- dist/promise.js
+++ dist/promise.js
@@ -89,7 +89,7 @@
      * @returns {boolean}
      */
     support: function() {
-      // IE MessageChannel slower than image error
+      // IE MessageChannel slower than script state change
       return !native(VBArray) && native(MessageChannel);
     },
     /**
