4156b9693f0d8f0192efbaca99b105d7d3f3cb8b
nuintun <nuintun@qq.com>
2018-04-08 16:26:50 +0800

    :art: Update files

--- dist/promise.js
+++ dist/promise.js
@@ -4,7 +4,7 @@
  * @license MIT
  * @version 0.0.1
  * @description A pure JavaScript ES6 promise polyfill.
- * @see https://nuintun.github.io/promise
+ * @see https://nuintun.github.io/promise#readme
  */
 
 (function () {
